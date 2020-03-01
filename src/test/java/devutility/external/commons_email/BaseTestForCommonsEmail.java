94aa6535fd70cf40b92c2ec41e98012c10d91dc7
Aldwin Su <fangyansu@163.com>
2020-03-01 23:57:26 +0800

    Upgrade version of devutility.external to 1.2.0 and self version to 1.0.1

--- src/test/java/devutility/external/commons_email/BaseTestForCommonsEmail.java
+++ src/test/java/devutility/external/commons_email/BaseTestForCommonsEmail.java
@@ -7,7 +7,7 @@ import devutility.internal.test.BaseTest;
 import devutility.internal.util.PropertiesUtils;
 
 public abstract class BaseTestForCommonsEmail extends BaseTest {
-	protected Properties properties = PropertiesUtils.getProperties("email.properties");
+	protected Properties properties = PropertiesUtils.getPropertiesFromResource("email.properties");
 	protected EmailHelper emailHelper;
 	protected String fromEmail = "tester@test.com";
 	protected String fromName = "Tester";
