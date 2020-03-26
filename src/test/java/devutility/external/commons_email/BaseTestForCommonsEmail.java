daa97eefaff27f76a973567b136a6c281e225c82
Aldwin Su <fangyansu@163.com>
2020-03-26 23:54:56 +0800

    Update following internal project.

--- src/test/java/devutility/external/commons_email/BaseTestForCommonsEmail.java
+++ src/test/java/devutility/external/commons_email/BaseTestForCommonsEmail.java
@@ -1,5 +1,6 @@
 package devutility.external.commons_email;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
@@ -7,12 +8,19 @@ import devutility.internal.test.BaseTest;
 import devutility.internal.util.PropertiesUtils;
 
 public abstract class BaseTestForCommonsEmail extends BaseTest {
-	protected Properties properties = PropertiesUtils.getPropertiesFromResource("email.properties");
 	protected EmailHelper emailHelper;
 	protected String fromEmail = "tester@test.com";
 	protected String fromName = "Tester";
 
 	public BaseTestForCommonsEmail() {
+		Properties properties = null;
+
+		try {
+			properties = PropertiesUtils.getPropertiesFromResource("email.properties");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
 		try {
 			emailHelper = EmailHelper.create(properties, "email");
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
